be859c7b9495104b5180ee9e7d563a1c72896a12
lucio <lucio@Lucio.fibertel.com.ar>
2026-05-29 21:12:37 -0300

    Limpieza de codigo. Contadores FPS y UPS.

--- src/main/java/main/Juego.java
+++ src/main/java/main/Juego.java
@@ -6,20 +6,24 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
-public class Juego extends JFrame{
+public class Juego extends JFrame implements Runnable{
     private Pantalla pantallaJuego;
-    
+    private final double FPS_SET = 60.0;
+    private final double UPS_SET = 60.0;
     private BufferedImage img;
     
-    public Juego(){
+    private Thread hiloJuego;
+    
+    public Juego(){ 
         importImg();
         
-        setSize(640,640);
-        setVisible(true);
+        setSize(640,480);
+        
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         pantallaJuego = new Pantalla(img);
         add(pantallaJuego);
+        setVisible(true);
     }
     
     private void importImg() {
@@ -34,6 +38,50 @@ public class Juego extends JFrame{
 
     public static void main(String[] args) {
         Juego juego = new Juego();
+        
+        juego.start();
+    }
+
+
+    @Override
+    public void run() {
+        double timePerFrame = 1000000000.0/ FPS_SET;
+        double timePerUpdate = 1000000000.0/ UPS_SET;
+        long lastFrame = System.nanoTime();
+        long lastTimeCheck = System.currentTimeMillis();
+        long lastUpdate = System.nanoTime();
+        int frames = 0;
+        int updates = 0;
+        
+        
+        
+        // Renderizado
+        while (true) {
+            long now = System.nanoTime();
+            if (now - lastFrame >= timePerFrame) { // Si el tiempo entre frames es mayor o igual al establecido
+                repaint(); // Redibujo la pantalla
+                lastFrame = now; // Actualizo ultimo frame
+                frames++;
+            }
+            // Actualizaciones
+            if (now - lastUpdate >= timePerUpdate) {
+                lastUpdate = now;
+                updates++;
+            }
+            // Estadisticas
+            
+            if (System.currentTimeMillis() - lastTimeCheck >= 1000) {
+                System.out.println("FPS: " + frames + "UPS: " + updates);
+                frames = 0;
+                updates = 0;
+                lastTimeCheck = System.currentTimeMillis();
+            }
+        }
+    }
+
+    private void start() {
+        hiloJuego = new Thread(this){};
+        hiloJuego.start();
     }
 
     
--- src/main/java/main/Pantalla.java
+++ src/main/java/main/Pantalla.java
@@ -7,6 +7,7 @@ package main;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import static java.lang.System.currentTimeMillis;
 import java.util.ArrayList;
 import javax.swing.JPanel;
 
@@ -16,9 +17,10 @@ import javax.swing.JPanel;
  */
 public class Pantalla extends JPanel {
 
+    //private Random random
     private BufferedImage img;
-    
     private ArrayList<BufferedImage> sprites = new ArrayList<>();
+    
     
     public Pantalla(BufferedImage img){
         this.img = img;
@@ -30,22 +32,23 @@ public class Pantalla extends JPanel {
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         
-        g.drawImage(sprites.get(1), 0, 0, null);
-        /*
-        g.setColor(Color.RED);
-        for(int x = 0;x<32;x++){
-        g.fillRect(x * 32, 0, 32, 32);
+        //g.drawImage(sprites.get(0), 0, 0, null);
+        
+        for(int x = 0;x<8;x++){
+            g.drawImage(sprites.get(x), x*28, 0, null);
+            
         }
-        */
+        repaint();
     }
+    
     // sprite peashooter en 169,13 en getSubimage
     private void loadSprites() {
-        for(int y=0;y<10;y++){
-            for(int x=0;x<10;x++){
-                sprites.add(img.getSubimage(x * 1, y * 1, 28, 32)); // Hay que rehacer la imagen de sprites
+        
+            for(int x=0;x<8;x++){
+                sprites.add(img.getSubimage(169+x*29,13, 28, 32)); // Hay que rehacer la imagen de sprites
                                                                     // para que esten alineados, asi se pueden recorrer
             }
-        }
+        
     }
     
 }
