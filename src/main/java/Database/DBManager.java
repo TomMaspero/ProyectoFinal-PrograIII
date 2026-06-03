637b753749d085f60945ef1a1f62281abc9e55ca
TomasMaspero <tmaspero@alboragro.com>
2026-06-03 19:53:41 -0300

    se agregan métodos de insert y delete en dbManager

--- src/main/java/Database/DBManager.java
+++ src/main/java/Database/DBManager.java
@@ -37,9 +37,45 @@ public class DBManager {
         
         return results;
     }
+
+    /**
+     * Ejecuta un INSERT y retorna la Primary Key autogenerada (ó -1 si falla)
+     */
+    public int runInsert(String sql, Object... params) {
+        try (PreparedStatement ps = dbConnect.getConnection()
+                .prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            setParams(ps, params);
+            ps.executeUpdate();
+            ResultSet keys = ps.getGeneratedKeys();
+            
+            if (keys.next()) {
+                return keys.getInt(1);
             }
-        } catch (SQLException sqlEx){
-            sqlEx.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return -1;
+    }
+
+    /**
+     * Ejecuta un UPDATE o DELETE y retorna el numero de filas afectadas
+     */
+    public int runUpdate(String sql, Object... params) {
+        try (PreparedStatement ps = dbConnect.getConnection().prepareStatement(sql)) {
+            setParams(ps, params);
+            
+            return ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return 0;
+    }
+
+    private void setParams(PreparedStatement ps, Object[] params) throws SQLException {
+        for (int i = 0; i < params.length; i++) {
+            ps.setObject(i + 1, params[i]);
         }
     }
 }
