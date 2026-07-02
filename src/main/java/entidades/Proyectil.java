afe9f9684ed339b7d7ff84f49dc9215ec51bd2e8
TomasMaspero <tmaspero@alboragro.com>
2026-07-01 23:26:35 -0300

    inicio de clase proyectil y combat manager

--- src/main/java/entidades/Proyectil.java
+++ src/main/java/entidades/Proyectil.java
@@ -1,8 +1,32 @@
 package entidades;
 
 public class Proyectil extends Entidad {
+    private int danio = 20;
+    private float velocidad = 2.0f;
     
     public Proyectil(float x, float y) {
-        super(x, y, 6); // chequear tamanio sprite
+        super(x, y, 15); // chequear tamanio sprite
+    }
+    
+    public void update() {
+        if (getX() < 640) { // limite pantalla (chequear numero)
+            mover(velocidad, 0);
+        }
+    }
+
+    public int getDanio() {
+        return danio;
+    }
+
+    public void setDanio(int danio) {
+        this.danio = danio;
+    }
+
+    public float getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(float velocidad) {
+        this.velocidad = velocidad;
     }
 }
---
+++ src/main/java/managers/CombatManager.java
@@ -0,0 +1,11 @@
+package managers;
+
+import entidades.Enemigo;
+import entidades.Proyectil;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombatManager {
+    List<Proyectil> proyectiles = new ArrayList<>();
+    List<Enemigo> enemigos = new ArrayList<>();
+}
